0f37c0ee84bfcea5bef78699a915ea1768cc149c
tangxu <km3@163.com>
2018-02-10 20:18:13 +0800

    first

---
+++ src/tx/huobi/Const.java
@@ -0,0 +1,11 @@
+package tx.huobi;
+
+public class Const {
+	public static String Access_Key = "xxxxxxxxxxxxxxxxxxxxxxxx";
+	
+	public static String Secret_Key = "xxxxxxxxxxxxxxxxxxxxxxxxx";
+	
+	public static String SignatureMethod = "HmacSHA256";
+	
+	public static String SignatureVersion = "2";
+}
